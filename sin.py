54d6a47c95f0ab0c3c48e4e3b5af1db609d9b7dd
RyomaOno-725 <rono.personal.22@gmail.com>
2023-12-14 02:01:02 +0900

    231214 Add plot

--- sin.py
+++ sin.py
@@ -1,2 +1,17 @@
 # Library
 import numpy as np
+import matplotlib.pyplot as plt
+import tkinter
+
+import matplotlib
+matplotlib.use('TkAgg')
+
+# define graph area
+width  = 10
+height = 5
+fig, ax = plt.subplots(figsize=(width,height))
+
+t = np.linspace(0, 100, 1000)  # x: time
+amp = np.sin(t)                # y: amplitude
+ax.plot(t,amp, color="red")   # plot
+plt.show()
