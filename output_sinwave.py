84383f10d2b59086500bfbf93ca8329aefe8597a
RyomaOno-725 <rono.personal.22@gmail.com>
2024-01-02 14:37:53 +0900

    refs #20240102 add make gif of sinwave

--- output_sinwave.py
+++ output_sinwave.py
@@ -1,4 +1,65 @@
 import numpy as np
-import math
 import matplotlib.pyplot as plt
+import colorsys as clrsys
+from PIL import Image
+import os
 
+# define
+radius = 1
+theta  = [np.radians(t) for t in range(0,720+1,10)]
+center = [-2,0]
+circle_x = np.cos(theta)               # x coordinate of unit-circle
+circle_y = np.sin(theta)               # y coordinate of unit-circle
+x1 = radius*circle_x + center[0]       # x coordinate of circle
+y1 = radius*circle_y                   # y coordinate of circle
+x2 = theta                             # x coordinate of sin wave
+y2 = y1                                # y coordinate of sin wave
+x3 = 0.3*circle_x + center[0]          # x coordinate of circle
+y3 = 0.3*circle_y                      # y coordinate of circle
+
+# color
+h, l, s = 5/255, 200/255, 0/255    # hue,luminance,saturation
+rgb1 = clrsys.hls_to_rgb(h, l, s)  # convert HLS to RGB
+h, l, s = 5/255, 160/255, 160/255  # hue,luminance,saturation
+rgb2 = clrsys.hls_to_rgb(h, l, s)  # convert HLS to RGB
+
+# define graph area
+width  = 20
+height = 2.2
+fig = plt.figure(figsize=(width,height),linewidth=0)
+l,b,w,h = 0,0,20/width,2.2/height # left,bottom,width,height
+ax = fig.add_axes((l,b,w,h),)
+
+# plot
+fp = "./Picture/"
+fn = "sinwave_ani"
+ex = ".jpg"
+fn_list = []
+for i in range(len(theta)):
+    plt.cla()
+    ax.plot(x1,y1,linestyle='None',marker='o',color=rgb1)                # plot circle
+    ax.plot(x2,y2,linestyle='None',marker='o',color=rgb1)                # plot sin wave
+    ax.plot([center[0],x1[0]], [center[1],y1[0]], color=rgb1,zorder=3)   # start line
+    ax.plot(x3[0:i],y3[0:i],color=rgb1,zorder=4)                         # plot circle2
+    ax.plot([center[0],x1[i]], [center[1],y1[i]], color=rgb2,zorder=5)   # theta
+    ax.plot([x1[i],x2[i]],[y1[i],y2[i]],marker='o',color=rgb2,zorder=6)  # plot line
+    fn_list.append(fp+fn+str(i)+ex)
+    fig.savefig(fp+fn+str(i)+ex)
+fn_list = [fn_list[0]]*100 + fn_list + [fn_list[-1]]*100
+
+# make animation
+ratio = 1
+rotate = 0
+list_images = []
+duration = 10    # [ms]
+for n in fn_list:
+    img = Image.open(n)
+    resized_img = img.resize((int(img.width*ratio), int(img.height*ratio)))
+    rotated_resized_img = resized_img.rotate(rotate)
+    list_images.append(rotated_resized_img)
+list_images[0].save('sinwave.gif',
+                    save_all=True, 
+                    append_images=list_images[1:],
+                    optimize=True, 
+                    duration=duration,
+                    loop=0)
--- sin_animation.py
+++ sin_animation.py
@@ -1,37 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import colorsys as clrsys
 
 # define graph area
-width  = 10
-height = 5
-fig, ax = plt.subplots(figsize=(width,height))
+width  = 20
+height = 2.2
+fig = plt.figure(figsize=[width,height])
+# ax = fig.add_axes([0,0,1,1])
+ax = fig.add_subplot(111)
 
-# define circle
+# define
 radius = 1
-theta  = [np.radians(t) for t in range(0,360+1,10)]
+theta  = [np.radians(t) for t in range(0,720+1,10)]
+center = [-2,0]
+circle_x = np.cos(theta)               # x coordinate of unit-circle
+circle_y = np.sin(theta)               # y coordinate of unit-circle
+x1 = radius*circle_x + center[0]       # x coordinate of circle
+y1 = radius*circle_y                   # y coordinate of circle
+x2 = theta                             # x coordinate of sin wave
+y2 = y1                                # y coordinate of sin wave
+x3 = 0.3*circle_x + center[0]          # x coordinate of circle
+y3 = 0.3*circle_y                      # y coordinate of circle
 
-# circle
-x1 = radius*np.cos(theta) - 2
-y1 = radius*np.sin(theta)
-# sin wave
-x2 = theta
-y2 = radius*np.sin(theta)
-
-ax.plot(x1,y1,color="red",linestyle='dashed')
+# color settings
+h, l, s = 5/255, 200/255, 0/255        # hue,luminance,saturation
+rgb1 = clrsys.hls_to_rgb(h, l, s)      # convert HLS to RGB
+h, l, s = 5/255, 160/255, 160/255      # hue,luminance,saturation
+rgb2 = clrsys.hls_to_rgb(h, l, s)      # convert HLS to RGB
 
 # plot
 ims = []
+# tile = 331
 for i in range(0,len(theta),1):
-    im = ax.plot(x1,y1,color="gray",linestyle='dashed')
-    im = ax.plot(x1[i],y1[i],'x')
-    im = ax.plot(x2,y2,color="gray",linestyle='dashed')
-    im = ax.plot(x2[i],y2[i],'x')
-    im = ax.plot([x1[i],x2[i]],[y1[i],y2[i]],color="red")
+    # ax = fig.add_subplot(tile)
+    im = None
+    im = ax.plot(x1,y1,marker='o',color=rgb1,zorder=1)       # plot circle
+    im = ax.plot(x2,y2,marker='o',color=rgb1,zorder=2)       # plot sin wave
+    # im = ax.plot([center[0],x1[0]], [center[1],y1[0]], color=rgb1,zorder=3)   # start line
+    # im = ax.plot(x3[0:i],y3[0:i],color=rgb1,zorder=4)                         # plot circle2
+    # im = ax.plot([center[0],x1[i]], [center[1],y1[i]], color=rgb2,zorder=5)   # theta
+    im = ax.plot([x1[i],x2[i]],[y1[i],y2[i]],marker='o',color=rgb2,zorder=6)  # plot line
     ims.append(im)
+    # tile += 1
+ims = [ims[0]]*100 + ims + [ims[-1]]*100
 
-interval = 10
+interval = 10  # [ms], fps=1/10x10^-3=100
 ani = animation.ArtistAnimation(fig, ims, interval=interval)
-plt.show()
+# plt.show()
 ani.save("output.gif",writer="imagemagick")
-plt.clf()
+# plt.clf()
